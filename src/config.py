f72d6165d70332e3f301243af4cc431bf05d366e
ouladhima <younes.amghar.pro@gmail.com>
2023-09-27 15:41:43 +0200

    Modification de config & rajout de multi-queries

--- src/config.py
+++ src/config.py
@@ -1,9 +1,20 @@
+liste_ville = open("liste_villes.txt" , 'r' , encoding='utf-8')
+liste_ville = [x.replace('\n','') for x in liste_ville.readlines()]
 
-queries = [
-    {
-        "keyword": "Ecole supérieur en ile-de-France  ",
-        "max_results" : 50,
-    },
-]
+queries = []
+for el in liste_ville[0:5]:
+    SingleQuery = {"keyword" : f"Ecole supérieur en {el}",
+                   "max_results" : 5}
+    queries.append(SingleQuery)
+    
 
-number_of_scrapers = 16
+queries = queries
+
+# queries = [
+#     {
+#         "keyword": "Ecole supérieur en ile-de-France  ",
+#         "max_results" : 50,
+#     },
+# ]
+
+number_of_scrapers = 12
